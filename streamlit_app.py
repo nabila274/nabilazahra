56be0987a3cdf8ae954c75e02d818145c2c38b75
nabila274 <nabilazahrakirananasution@gmail.com>
2025-04-23 15:08:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 welcome gais")
+st.title("🎈 welcome ges")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
