99edcd970b8a77695155230487d17a97797921d1
nabila274 <nabilazahrakirananasution@gmail.com>
2025-05-21 14:41:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 welcome ges")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("IMG-20250429-WA0059.jpg",width=200)
